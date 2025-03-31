7b9a781b812b7f4f5a1e66d067f73c0a735d4d1f
vellun <moonpassiflora@gmail.com>
2025-03-31 12:34:16 +0300

    [fix] add link at logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,16 +4,19 @@ import Text from "components/Text";
 import cn from "classnames";
 
 import styles from "./Navbar.module.scss";
+import { Link } from "react-router";
 
 const Navbar = () => {
   return (
     <div className={cn(styles.navbar)}>
-      <div className={cn(styles.navbarLogo)}>
-        <img src={githubLogo} alt="GitHub User Logo" width="32px" height="32px" />
-        <Text weight="bold" view="p-20">
-          GitHub Client
-        </Text>
-      </div>
+      <Link className="link" to={`/repositories`}>
+        <div className={cn(styles.navbarLogo)}>
+          <img src={githubLogo} alt="GitHub User Logo" width="32px" height="32px" />
+          <Text weight="bold" view="p-20">
+            GitHub Client
+          </Text>
+        </div>
+      </Link>
       <img src={userLogo} alt="GitHub Logo" width="32px" height="32px" />
     </div>
   );
