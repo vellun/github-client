10abb0d16e31cde2c396b7db07b712ed5cf71e89
vellun <moonpassiflora@gmail.com>
2025-04-09 05:04:39 +0300

    [feat] back button

--- src/App/pages/RepoDetailPage/components/TitleSection/TitleSection.tsx
+++ src/App/pages/RepoDetailPage/components/TitleSection/TitleSection.tsx
@@ -2,6 +2,7 @@ import { ArrowDownIcon } from "components/icons/ArrowDownIcon";
 import { Text } from "components/Text";
 import styles from "./titleSection.module.scss";
 import { observer } from "mobx-react-lite";
+import { useNavigate } from "react-router";
 
 interface TitleSectionProps {
   avatarUrl?: string;
@@ -9,9 +10,17 @@ interface TitleSectionProps {
 }
 
 export const TitleSection: React.FC<TitleSectionProps> = observer(({ avatarUrl, repoName }) => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div className={styles.root}>
-      <ArrowDownIcon className={styles.root__icon} width={32} height={32}></ArrowDownIcon>
+      <button className={styles.root__button} onClick={handleGoBack}>
+        <ArrowDownIcon className={styles.root__icon} width={32} height={32}></ArrowDownIcon>
+      </button>
       <img className={styles.root__image} src={avatarUrl} alt="GitHub Repo Logo" width="40px" height="40px" />
       <Text tag="h1" weight="bold" color="primary" view="title">
         {repoName}
